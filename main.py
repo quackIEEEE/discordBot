6b74367a45aadf37fc86b2683cc9e19ec1628d56
quackIEEEE <121175295+quackIEEEE@users.noreply.github.com>
2025-09-18 15:40:11 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import discord
+import os
+import datetime
+from dotenv import load_dotenv
+from discord.ext import commands
+
+
+load_dotenv()
+
+token = os.getenv("DISCORD_BOT_TOKEN")
+# intents是要求機器人的權限
+intents = discord.Intents.all()
+# command_prefix是前綴符號，可以自由選擇($, #, &...)
+bot = commands.Bot(command_prefix = "/", intents = intents)
+TARGET_DATE = datetime.date(2026, 2, 1)
+
+@bot.event
+# 當機器人完成啟動
+async def on_ready():
+    print(f"目前登入身份 --> {bot.user}")
+
+@bot.command()
+
+
+async def countdown(ctx):
+    today = datetime.date.today()
+    delta = (TARGET_DATE - today).days
+    await ctx.send(f"距離2026年2月1日還有{delta}天，考試加油:)")
+
+
+bot.run(token)
